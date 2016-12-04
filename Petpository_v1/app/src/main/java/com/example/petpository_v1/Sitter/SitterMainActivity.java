27a3ed74319c1e49c45bc342316e5d6fca9cb7aa
amliomnie <mimiheadmeng@gmail.com>
2016-12-04 17:31:50 +0700

    Activity pending core xml

--- Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/SitterMainActivity.java
+++ Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/SitterMainActivity.java
@@ -1,15 +1,34 @@
 package com.example.petpository_v1.Sitter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.petpository_v1.R;
 
 public class SitterMainActivity extends AppCompatActivity {
 
+    private Button btn_gotoPending;
+    private String placeId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitter_main);
+
+        //goto pending ... mimi
+        btn_gotoPending = (Button) findViewById(R.id.btn_gotoPending);
+        btn_gotoPending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoPending = new Intent(SitterMainActivity.this, PendingActivity.class);
+                placeId = "123";
+                gotoPending.putExtra("placeId", placeId);
+                startActivity(gotoPending);
+            }
+        });
+
     }
 }
