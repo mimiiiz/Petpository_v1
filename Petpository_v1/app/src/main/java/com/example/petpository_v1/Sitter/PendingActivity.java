3c7c8ef5b5dbcdee8694dfdb8277fd4a9ea903ea
amliomnie <mimiheadmeng@gmail.com>
2016-12-05 00:22:15 +0700

    edit model requse , can set Pending w/bug
    edit start,end date to string

--- Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/PendingActivity.java
+++ Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/PendingActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.petpository_v1.Model.Pet;
 import com.example.petpository_v1.Model.RequestPet;
 import com.example.petpository_v1.R;
 import com.google.firebase.database.DataSnapshot;
@@ -31,6 +33,7 @@ public class PendingActivity extends AppCompatActivity {
     protected DatabaseReference mDatabase;
     private RequestPet requestPetObj;
     private ArrayList<RequestPet> requestPetArrayList;
+    private Pet petObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,7 @@ public class PendingActivity extends AppCompatActivity {
         Intent intent = getIntent();
         requestPlaceId = intent.getStringExtra("placeId");
 
-        requestPetArrayList = new ArrayList<RequestPet>();
+//        requestPetArrayList = new ArrayList<RequestPet>();
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         Query mQ = mDatabase.child("RequestPet").orderByChild("requestTimeStamp");
@@ -50,41 +53,35 @@ public class PendingActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot mSnap : dataSnapshot.getChildren()) {
                     requestPetObj = new RequestPet();
-                    Log.d("in for >>>>>>>>>>", "............");
 
                     //check place id same selected && status = pending
                     if (mSnap.child("requestPlaceID").getValue().toString().equals(requestPlaceId)
                             && mSnap.child("requestStatus").getValue().toString().equals("pending")) {
-                        Log.d("in if >>>>>>>>>>", "............");
 
                         requestPetObj.setRequestID(mSnap.child("requestID").getValue().toString());
                         requestPetObj.setRequestUID_owner(mSnap.child("requestUID_owner").getValue().toString());
                         requestPetObj.setRequestUID_sitter(mSnap.child("requestUID_sitter").getValue().toString());
-                        requestPetObj.setRequestUID_owner(mSnap.child("requestStatus").getValue().toString());
+                        requestPetObj.setRequestStatus(mSnap.child("requestStatus").getValue().toString());
                         requestPetObj.setRequestPlaceID(mSnap.child("requestPlaceID").getValue().toString());
                         requestPetObj.setRequestPetID(mSnap.child("requestPetID").getValue().toString());
+
+                        requestPetObj.setRequestStartDate(mSnap.child("requestStartDate").getValue().toString());
+                        requestPetObj.setRequestEndDate(mSnap.child("requestEndDate").getValue().toString());
 
                         try {
                             DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 
                             String reqTimeStampString = mSnap.child("requestTimeStamp").getValue().toString();
                             Date reqTimeStamp = df.parse(reqTimeStampString);
-
-                            String reqStartString = mSnap.child("requestStartDate").getValue().toString();
-                            Date reqStartDate = df.parse(reqStartString);
-
-                            String reqEndString = mSnap.child("requestEndDate").getValue().toString();
-                            Date reqEndDate = df.parse(reqEndString);
-
                             requestPetObj.setRequestTimeStamp(reqTimeStamp);
-                            requestPetObj.setRequestStartDate(reqStartDate);
-                            requestPetObj.setRequestEndDate(reqEndDate);
+
 
                         } catch (ParseException e) {
                             e.printStackTrace();
                         } //end catch
-                        requestPetArrayList.add(requestPetObj);
-                        Log.d("ADDDDDDDDDDD >", "............");
+                        petObj = createPet(requestPetObj.getRequestPetID(), requestPetObj.getRequestUID_owner());
+
+//                        requestPetArrayList.add(requestPetObj);
 
                     } //end if
                 } // end for
@@ -96,12 +93,65 @@ public class PendingActivity extends AppCompatActivity {
             }
         }); //end value listener
 
-        Log.d("arrr >>>>>>>>>>", requestPetArrayList.size() + "");
-
-        for (RequestPet requestPet : requestPetArrayList){
-            Log.d("Pet>>>>>", requestPet.getRequestID());
-        }
-
 
     }
+
+    protected Pet createPet(String requestPetID, String requestUID_owner){
+        Log.d("UID Owner>> ", requestPetObj.getRequestUID_owner());
+        Log.d("Pet Id >> ", requestPetObj.getRequestPetID());
+
+
+        petObj = new Pet();
+        Query mQ_getPet = mDatabase.child("Owner").child(requestPetObj.getRequestUID_owner()).child("Pet").child(requestPetObj.getRequestPetID()).orderByValue();
+        mQ_getPet.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Pet>>> ", dataSnapshot.toString());
+                petObj.setPetID(dataSnapshot.child("petID").getValue().toString());
+                petObj.setPetName(dataSnapshot.child("petName").getValue().toString());
+                petObj.setPetType(dataSnapshot.child("petType").getValue().toString());
+                petObj.setPetSize(dataSnapshot.child("petSize").getValue().toString());
+                setTextView();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return petObj;
+    }
+
+    protected void setTextView(){
+
+        requestPetObj.getRequestPetID();
+
+
+        tv_requestPetName = (TextView) findViewById(R.id.tv_requestPetName);
+        tv_requestPetSize = (TextView) findViewById(R.id.tv_requestPetSize);
+        tv_requestPetType = (TextView) findViewById(R.id.tv_requestPetType);
+        tv_requestOwnerEmail = (TextView) findViewById(R.id.tv_requestOwnerEmail);
+        tv_requestStartDate = (TextView) findViewById(R.id.tv_requestStartDate);
+        tv_requestEndDate = (TextView) findViewById(R.id.tv_requestEndDate);
+
+        tv_requestPetName.setText(petObj.getPetName());
+        tv_requestPetType.setText(petObj.getPetType());
+        tv_requestPetSize.setText(petObj.getPetSize());
+        tv_requestStartDate.setText(requestPetObj.getRequestStartDate().toString());
+        tv_requestEndDate.setText(requestPetObj.getRequestEndDate().toString());
+
+
+
+//
+//        btn_accept = (Button) findViewById(R.id.btn_accept);
+//        btn_accept.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+
 }
