c7d25f550ea1d25c4aeb44b61b9fe3e6739f56ec
Marktrs <marktrs69@gmail.com>
2016-12-04 17:32:38 +0700

    Finished loginWithGoogle
    - Fix Authentication fail cause .jason is invalid file
    - Create ProfileActivity
    - Create MainActivity to select role before enter app

--- Petpository_v1/app/src/main/java/com/example/petpository_v1/MainActivity.java
+++ Petpository_v1/app/src/main/java/com/example/petpository_v1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.petpository_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Petpository");
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
     }
 }
