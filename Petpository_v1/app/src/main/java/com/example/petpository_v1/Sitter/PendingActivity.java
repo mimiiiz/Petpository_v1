290eafe3d63ad12fc506b20b09455157a8855dde
amliomnie <mimiheadmeng@gmail.com>
2016-12-05 15:33:47 +0700

    can show pet image

--- Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/PendingActivity.java
+++ Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/PendingActivity.java
@@ -1,22 +1,31 @@
 package com.example.petpository_v1.Sitter;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.petpository_v1.Model.Pet;
 import com.example.petpository_v1.Model.RequestPet;
 import com.example.petpository_v1.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import java.util.ArrayList;
 
 public class PendingActivity extends AppCompatActivity {
@@ -27,6 +36,8 @@ public class PendingActivity extends AppCompatActivity {
     protected DatabaseReference mDatabase;
     private RequestPet requestPetObj;
     public ArrayList<RequestPet> requestPetsArray;
+    private FirebaseStorage mStorage;
+    private ImageView imgv_petPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,7 @@ public class PendingActivity extends AppCompatActivity {
 
     protected void setTextView(RequestPet requestPetObj){
 
+        imgv_petPic = (ImageView) findViewById(R.id.imgv_petRequest);
         tv_requestPetName = (TextView) findViewById(R.id.tv_requestPetName);
         tv_requestPetSize = (TextView) findViewById(R.id.tv_requestPetSize);
         tv_requestPetType = (TextView) findViewById(R.id.tv_requestPetType);
@@ -56,9 +68,24 @@ public class PendingActivity extends AppCompatActivity {
         tv_requestStartDate.setText(requestPetObj.getRequestStartDate());
         tv_requestEndDate.setText(requestPetObj.getRequestEndDate());
 
+        mStorage = FirebaseStorage.getInstance();
+        StorageReference storageRef = mStorage.getReferenceFromUrl("gs://petpository-d8def.appspot.com");
+        storageRef.child(petObj.getPetID()+"/1").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(getApplicationContext()).load(uri).fitCenter().centerCrop().into(imgv_petPic);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e("Slid", exception.getMessage());
+            }
+        });
+
+
     }
 
-    public void sentRequest(View view){
+    public void sentApprove(View view){
         switch (view.getId()){
             case R.id.btn_accept:
                 mDatabase = FirebaseDatabase.getInstance().getReference();
