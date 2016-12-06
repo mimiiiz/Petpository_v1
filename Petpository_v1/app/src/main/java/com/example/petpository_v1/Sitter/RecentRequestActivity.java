c530b9c1bdefdf490b814e799e5806aca0357f7a
Jessegade <s7070068@kmitl.ac.th>
2016-12-06 19:52:44 +0700

    edit dialog

--- Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/RecentRequestActivity.java
+++ Petpository_v1/app/src/main/java/com/example/petpository_v1/Sitter/RecentRequestActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -120,6 +121,7 @@ public class RecentRequestActivity extends AppCompatActivity implements RecentRe
     public void onClick(final int position) {
         Log.d("aaaa", "aaaaa");
         final Dialog dialog = new Dialog(RecentRequestActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.recentpet_dialog);
 
         final ImageView dogImg = (ImageView) dialog.findViewById(R.id.dogimg);
@@ -139,6 +141,13 @@ public class RecentRequestActivity extends AppCompatActivity implements RecentRe
         teltv.setText(requests.get(position).getOwnerPhoneNo());
         teltv.setSelected(true);
         ImageButton telOwner = (ImageButton)dialog.findViewById(R.id.telowner);
+        ImageButton closebt = (ImageButton)dialog.findViewById(R.id.closebt);
+        closebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
 
         if(requests.get(position).getRequestStartDate().equals(requests.get(position).getRequestEndDate())){
             to.setVisibility(View.GONE);
@@ -154,13 +163,6 @@ public class RecentRequestActivity extends AppCompatActivity implements RecentRe
             }
         });
 
-        Button close = (Button)dialog.findViewById(R.id.close);
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
 
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -187,6 +189,8 @@ public class RecentRequestActivity extends AppCompatActivity implements RecentRe
                 Glide.with(getApplicationContext()).load(uri).fitCenter().centerCrop().into(dogImg);
             }
         });
+
+
         dialog.show();
     }
 
